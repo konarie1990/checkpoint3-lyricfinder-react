561e5da4feaa4e45a077f53adc46f85e2bc5bb71
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-01-30 21:23:34 -0600

    Updates

---
+++ precache-manifest.dc18d9ac4d51b53962ed50b07a0bd6d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08546579e24cf2b2b56e",
+    "url": "/checkpoint3-lyricfinder-react/static/js/runtime~main.08546579.js"
+  },
+  {
+    "revision": "ac31184afa84cce27ced",
+    "url": "/checkpoint3-lyricfinder-react/static/js/main.ac31184a.chunk.js"
+  },
+  {
+    "revision": "c33d6651ba05a87d9a33",
+    "url": "/checkpoint3-lyricfinder-react/static/js/1.c33d6651.chunk.js"
+  },
+  {
+    "revision": "ac31184afa84cce27ced",
+    "url": "/checkpoint3-lyricfinder-react/static/css/main.34a994d1.chunk.css"
+  },
+  {
+    "revision": "6f005a9c2b8289fa6b9436b4b9b18236",
+    "url": "/checkpoint3-lyricfinder-react/index.html"
+  }
+];
